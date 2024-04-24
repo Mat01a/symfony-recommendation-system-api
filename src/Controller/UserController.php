5b4a24e70068e61c135b4798f26afb6a3657e4f1
Mateusz <matolszakowski@gmail.com>
2024-04-24 18:54:15 +0200

    Registration added

---
+++ src/Controller/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/register', name: 'app_user')]
+    public function store(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $requestBody = json_decode($request->getContent(), false);
+
+        if (!$requestBody->email)
+        return $this->json([
+            'message' => 'You need to specify email'
+        ], 401);
+
+        if (!$requestBody->password)
+        {
+            return $this->json([
+                'message' => 'You need to specify password'
+            ], 401);
+        } 
+
+        try
+        {
+            $user = new User();
+            $user->setEmail($requestBody->email);
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $requestBody->password
+            );
+
+            $user->setPassword($hashedPassword);
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+        catch(\Exception $e)
+        {
+            return $this->json([
+                'message' => 'This email is already used'
+            ], 400);
+        }
+
+        return $this->json([
+            'message' => 'Your account has been created successfully'
+        ]);
+
+            return $this->json([
+                'message' => 'Welcome to your new controller!',
+                'path' => 'src/Controller/UserController.php',
+            ]);
+        }
+}
