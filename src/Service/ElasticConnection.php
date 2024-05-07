7103bab7342243235eb1aedf05b5c24f29c139d8
Mateusz <matolszakowski@gmail.com>
2024-05-07 14:57:02 +0200

    Fixed directory typo

---
+++ src/Service/ElasticConnection.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use Elastic\Elasticsearch\ClientBuilder;
+
+class ElasticConnection
+{
+    private $login;
+    private $password;
+    private $ca;
+    public function __construct($elasticLogin, $elasticPassword, $elasticCA)
+    {
+        $this->login = $elasticLogin;
+        $this->password = $elasticPassword;
+        $this->ca = $elasticCA;
+    }
+
+    public function connection()
+    {
+        $client = ClientBuilder::create()
+            ->setHosts(['https://localhost:9200'])
+            ->setBasicAuthentication($this->login, $this->password)
+            ->setCABundle($this->ca)
+            ->build();
+        return $client;
+    }
+
+    public function addIndex(string $name, int $id, string $index = 'product')
+    {
+        # Check if is TEST env, if is change index to test
+        if ($_ENV["APP_ENV"] === "test")
+            $index = 'test_product';
+
+        $client = $this->connection();
+        $params = [
+            'index' => $index,
+            'id' => $id,
+            'body' => [ 
+                'name' => $name
+                ]
+        ];
+
+        $response = $client->index($params);
+
+        return $response;
+    }
+
+}
+
+?>
